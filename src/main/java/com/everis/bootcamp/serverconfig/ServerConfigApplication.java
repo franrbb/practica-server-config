b2c44417d7c3f19985a198d7277bfcfd2c9a259d
Francisco <fran89rbb@hotmail.com>
2021-11-29 11:47:58 +0100

    Actualizando fichero yml

--- src/main/java/com/everis/bootcamp/serverconfig/ServerConfigApplication.java
+++ src/main/java/com/everis/bootcamp/serverconfig/ServerConfigApplication.java
@@ -2,7 +2,9 @@ package com.everis.bootcamp.serverconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ServerConfigApplication {
 
